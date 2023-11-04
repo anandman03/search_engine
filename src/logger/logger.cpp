901cf07ecca3c648aa1f82e3e8d1c6c1d5be3b65
anandman03 <anandmansimar@gmail.com>
2023-11-04 14:31:41 +0530

    add levels to logger

--- src/logger/logger.cpp
+++ src/logger/logger.cpp
@@ -2,16 +2,24 @@
 
 namespace logger {
 
+void Logger::log_debug(const std::string& msg_str) noexcept {
+    if (log_level >= LogLevel::DEBUG) log_message("DEBUG", msg_str);
+}
+
 void Logger::log_error(const std::string& msg_str) noexcept {
-    std::cout << "[ERROR]: " << msg_str << std::endl;
+    if (log_level >= LogLevel::ERROR) log_message("ERROR", msg_str);
 }
 
 void Logger::log_info(const std::string& msg_str) noexcept {
-    std::cout << "[INFO]: " << msg_str << std::endl;
+    if (log_level >= LogLevel::INFO) log_message("INFO", msg_str);
 }
 
 void Logger::log_warn(const std::string& msg_str) noexcept {
-    std::cout << "[WARN]: " << msg_str << std::endl;
+    if (log_level >= LogLevel::WARN) log_message("WARN", msg_str);
+}
+
+void Logger::log_message(const std::string& msg_type, const std::string& msg_str) {
+    std::cout << "[" << msg_type << "]: " << msg_str << std::endl;
 }
 
 }
--- src/logger/logger.h
+++ src/logger/logger.h
@@ -6,14 +6,22 @@
 
 namespace logger {
 
+enum class LogLevel {ERROR=1, WARN, INFO, DEBUG};
+
 class Logger {
 public:
     Logger() = default;
     ~Logger() = default;
 
+    inline static LogLevel log_level = LogLevel::WARN;
+
+    static void log_debug(const std::string& msg_str) noexcept;
     static void log_error(const std::string& msg_str) noexcept;
     static void log_info(const std::string& msg_str) noexcept;
     static void log_warn(const std::string& msg_str) noexcept;
+
+private:
+    static void log_message(const std::string& msg_type, const std::string& msg_str);
 };
 
 #define LOG_ERROR(STR) Logger::log_error(STR)
